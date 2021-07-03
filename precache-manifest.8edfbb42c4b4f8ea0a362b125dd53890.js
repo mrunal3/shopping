a42d4862e972cd06d787b42ec8e7bf4e2f71251a
Mrunal Alatkar <mrunal500@gmail.com>
2021-07-04 00:09:11 +0530

    Updates

---
+++ precache-manifest.8edfbb42c4b4f8ea0a362b125dd53890.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ef079b9144f8a21b72ca3fd8fda3c6e",
+    "url": "/shopping/index.html"
+  },
+  {
+    "revision": "e24ecde8c30d74bde617",
+    "url": "/shopping/static/js/2.885d86e0.chunk.js"
+  },
+  {
+    "revision": "89a1b2dcd30c03705b2bceeb141b76b6",
+    "url": "/shopping/static/js/2.885d86e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6fdc95da4f28ba0cc81",
+    "url": "/shopping/static/js/main.bc1d10d5.chunk.js"
+  },
+  {
+    "revision": "da50e44b7c43c9a279fc",
+    "url": "/shopping/static/js/runtime-main.b6ee471e.js"
+  }
+]);
